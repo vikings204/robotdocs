1f42b92454e9b34bf7f5baaccd846d804bae80e5
Nevadex <nevadex@gmail.com>
2024-02-13 21:00:00 -0500

    still doesnt make rev work, making it a nonimportant task now

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -26,7 +29,35 @@ func main() {
 	}
 
 	//panic(fixExternalRefs("htmltest\\baller\\og.html", "htmltest\\baller\\_external"))
-	fmt.Println(fixExternalRefs("htmltest\\baller\\og.html", "htmltest\\baller\\_external"))
+	fmt.Println(fixExternalRefs("og.html", "htmltest\\baller", "htmltest\\baller\\_external"))
+	//fmt.Println(fixExternalRefs("app.gitbook.com_public_app_public-PO7REWUB.min.js-v-10.9.593-82f7df415a98c42348302ad74af126b6adfbf759-7828934982.js", "htmltest\\baller\\_external", "htmltest\\baller\\_external"))
+
+	for i := 0; i < 1; i++ {
+		fmt.Println(filepath.WalkDir("htmltest\\baller\\_external", func(path string, di fs.DirEntry, err error) error {
+			if strings.Contains(path, ".js") {
+				fmt.Println(path)
+				return fixExternalRefs(path, "", "htmltest\\baller\\_external")
+			} else {
+				return nil
+			}
+		}))
+	}
+	fmt.Println(filepath.WalkDir("htmltest\\baller\\_external", func(path string, di fs.DirEntry, err error) error {
+		if strings.Contains(path, ".js") {
+			fmt.Println(path)
+			bytes, er := os.ReadFile(path)
+			if er != nil {
+				return er
+			}
+			str := string(bytes)
+
+			str = strings.Replace(str, "https://app.gitbook.com/public/app/chunks/", "http://127.0.0.1:3000/baller/_external/app.gitbook.com_public_app_chunks_", -1)
+
+			return os.WriteFile(path, []byte(str), os.ModePerm)
+		} else {
+			return nil
+		}
+	}))
 
 	err = InitializeBinary()
 	if err != nil {
